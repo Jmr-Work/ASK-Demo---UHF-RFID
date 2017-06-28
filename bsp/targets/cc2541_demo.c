963d016a23141d99c866dc774eb73bf5ebefa50e
Justin Reina <justinmreina@gmail.com>
2017-06-28 12:43:43 -0700

    (B)    fails free edition for code size compile
    `

--- bsp/targets/cc2541_demo.c
+++ bsp/targets/cc2541_demo.c
@@ -1,91 +1,50 @@
 #include "cc2541_demo.h"
 
-//#define   DEMO_OPTION_ONE     (1)                                         // simple Tx
-#define   DEMO_OPTION_TWO     (1)                                         //  part number verification (manual call)
-//#define     DEMO_OPTION_THREE     (1)                                       //  part number verification (api call)
-
-
-#if defined(DEMO_OPTION_TWO) || defined(DEMO_OPTION_THREE)
-    volatile uint8_t rx_val = 0x00;
-#endif
 
 
 /************************************************************************************************************************************/
 /** @fcn        void cc2541_demo(void)
  *  @brief      x
  *  @details    x
+ *
+ *  @section    Waveform (see signal.c:signal_generate()
+ *      Pre: .        Idle-Off    []
+ *      [0]: (200 us) Idle-CW     [200us]
+ *      [1]: .        Select      [3162.5us]
+ *      [2]: (200 us) Idle-CW     [200us]
+ *      [3]: .        Query       [1425us]
+ *      [4]: (400 ms) Idle-CW     [400us]
+ *      [5]: .        Idle-Off    [1000us]
  */
 /************************************************************************************************************************************/
 void cc2541_demo(void) {
   
-    uint16_t i = 0x00;
-        
-    asm(" NOP");                                                            /* placeholder for dbg breakpoint                       */
-    
+    uint16_t loop_ct = 0;
+
+    //Generate Waveform
+//! signal_generate();
+    signal_grab_next();
+
+    radio_run_init();
+
     for(;;) {
-      
-#ifdef DEMO_OPTION_ONE      
-        mcu_radio_spi_tx(0x55);
-        
-        //Small Delay
-        for(i=0; i<50; i++) {
-            asm(" NOP");
-        }
-#endif  
-#ifdef DEMO_OPTION_TWO
 
-        //CS LOW
-        P1 &= ~BIT7;                                                        /* Misc. Dbg Pin                                        */
-        RADIO_PORT &= ~RADIO_PIN_CS_N;                                      /* CS to begin comm                                     */
-        
-        while(RADIO_PORT & RADIO_PIN_MISO);
-        
-        U0DBUF = (RADIO_BURST_ACCESS|RADIO_READ_ACCESS)|0x2F;
-        
-        // Check if byte is transmitted
-        while(!(U0CSR & U0CSR_TX_BYTE));
+        //DBG-1 HIGH
 
-        // Clear transmit byte status
-        U0CSR &= ~U0CSR_TX_BYTE;
-        
-        U0DBUF = 0x8F;
-        
-        // Check if byte is transmitted
-        while(!(U0CSR & U0CSR_TX_BYTE));
+        radio_run_loop();
 
-        // Clear transmit byte status
-        U0CSR &= ~U0CSR_TX_BYTE;
+        signal_grab_next();
 
-        //Grab PartNumber
-        U0DBUF = 0x00;
-            
-        // Check if byte is transmitted
-        while(!(U0CSR & U0CSR_TX_BYTE));
+        radio_run_waitForRoom();
 
-        // Clear transmit byte status
-        U0CSR &= ~U0CSR_TX_BYTE;
+        //DBG-1 LOW
 
-        //Get Val
-        rx_val = U0DBUF;
-        
-        //CS HIGH
-        RADIO_PORT |= RADIO_PIN_CS_N;
-        P1 |= BIT7;    
-        
-        
-        //Small Delay
-        for(i=0; i<50000; i++) {
-            asm(" NOP");                                                    /* set to 20 kHz                                        */
-        }
-#endif
-#ifdef DEMO_OPTION_THREE
-        cc12xxSpiReadReg(CC120X_PARTNUMBER, (uint8_t *)&rx_val, 1);
-        
-        //Small Delay
-        for(i=0; i<50000; i++) {
-            asm(" NOP");                                                    /* set to 20 kHz                                        */
-        }
-#endif        
-    }
+        radio_run_prepForNext();
+
+        loop_ct++;
+
+        //DBG-2 TOGGLE
+
+    } /* end for(;;)            */
 }
 
